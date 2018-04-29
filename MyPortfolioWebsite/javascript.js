d2e30653764e6057147835905ac5a1ba471cae50
kagznjenga <38765367+kagznjenga@users.noreply.github.com>
2018-04-29 05:08:53 -0400

    Add files via upload

--- MyPortfolioWebsite/javascript.js
+++ MyPortfolioWebsite/javascript.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
+//JQuery code for initiating a click event
+$(document).ready(function () {
+    //line 4 and 5 hides the elements with their corresponding classnames
     $('.scratch').hide();
+    $('.caesarCipher').hide();
+    /**The following code shows the hidden content when the declared button is clicked*/
     $('.projects-button').click(function() {
-        $(this).next().slideToggle(400);
+        $(this).nextAll().slideToggle(400);
         $(this).toggleClass('active');
     });
 });
